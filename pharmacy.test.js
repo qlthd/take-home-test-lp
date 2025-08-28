788c4599bd71fe213138cdaba84b3099a8a5fb44
Quentin Lathaud <quentin.lathaud@groupeseloger.com>
2025-08-28 22:59:26 +0200

    add: different drugs test for pharmacy

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,34 @@ describe("Pharmacy", () => {
       [new Drug("test", 1, 2)],
     );
   });
+
+  it("Magic Pill benefit and expiresIn never change", () => {
+    expect(
+      new Pharmacy([new Drug("Magic Pill", 5, 40)]).updateBenefitValue(),
+    ).toEqual([new Drug("Magic Pill", 5, 40)]);
+  });
+
+  it("Fervex increase benefit by 3 when expiresIn < 6", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 5, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", 4, 6)]);
+  });
+
+  it("Fervex benefit drops to 0 after expiration", () => {
+    expect(
+      new Pharmacy([new Drug("Fervex", 0, 10)]).updateBenefitValue(),
+    ).toEqual([new Drug("Fervex", -1, 0)]);
+  });
+
+  it("Herbal Tea increases benefit and decreases expiresIn", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 2, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Herbal Tea", 1, 4)]);
+  });
+
+  it("Herbal Tea increases benefit twice as fast after expiration", () => {
+    expect(
+      new Pharmacy([new Drug("Herbal Tea", 0, 3)]).updateBenefitValue(),
+    ).toEqual([new Drug("Herbal Tea", -1, 5)]);
+  });
 });

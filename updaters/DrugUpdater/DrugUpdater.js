d3fe45b0b74a927bde6d1cad4dca90e0501652f1
Quentin Lathaud <quentin.lathaud@groupeseloger.com>
2025-08-28 22:08:26 +0200

    add: base class DrugUpdater

---
+++ updaters/DrugUpdater/DrugUpdater.js
@@ -0,0 +1,39 @@
+export class DrugUpdater {
+  constructor(drug) {
+    this.drug = drug;
+  }
+
+  dailyUpdate() {
+    this.updateBenefit();
+    this.decrementExpiresIn();
+    this.handleExpiration();
+    this.handleBenefitsBounds();
+  }
+
+  updateBenefit() {
+    this.decreaseBenefit();
+  }
+
+  increaseBenefit(amount = 1) {
+    this.drug.benefit += amount;
+  }
+
+  decreaseBenefit(amount = 1) {
+    this.drug.benefit -= amount;
+  }
+
+  decrementExpiresIn() {
+    this.drug.expiresIn -= 1;
+  }
+
+  handleExpiration() {
+    if (this.drug.expiresIn < 0) {
+      this.decreaseBenefit();
+    }
+  }
+
+  handleBenefitsBounds() {
+    if (this.drug.benefit < 0) this.drug.benefit = 0;
+    if (this.drug.benefit > 50) this.drug.benefit = 50;
+  }
+}

9146701851db2efe79b52062ed6514a6f8ce67bc
Quentin Lathaud <quentin.lathaud@groupeseloger.com>
2025-08-28 23:17:20 +0200

    fix index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const pharmacy = new Pharmacy(drugs);
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.parse(JSON.stringify(pharmacy.updateBenefitValue())));
+  log.push(
+    JSON.parse(JSON.stringify(pharmacy.updateBenefitAndExpiresInValues())),
+  );
 }
 
 /* eslint-disable no-console */
